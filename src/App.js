adf2b465b643f313221e4d21c3920ea8b0d55588
Dmitriy Pidko <pidkodima@gmail.com>
2024-05-21 13:27:03 +0300

    tests

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 		<div className='App'>
 			<Header></Header>
 			<Routes>
-				<Route path={''} element={<ProductList />} />
+				<Route path='/' element={<ProductList />} />
 				<Route path={'form'} element={<Form />} />
 			</Routes>
 		</div>
