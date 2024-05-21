f5e47a525674ca721070272a4d7e1fb0d5ac83e8
Dmitriy Pidko <pidkodima@gmail.com>
2024-05-21 11:16:11 +0300

    tests

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+// import OpenAI from 'openai'
 import { useEffect } from 'react'
 import './App.css'
+
 const tg = window.Telegram.WebApp
+// const openai = new OpenAI({
+// 	apiKey: 'sk-proj-SfG2qEWz36GZhSSinZz4T3BlbkFJwr2x6rO1QetoDWPO2e93',
+// 	dangerouslyAllowBrowser: true,
+// })
 
 function App() {
 	useEffect(() => {
